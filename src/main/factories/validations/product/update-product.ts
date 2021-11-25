53d493a4df5dcf3f99ae85c1ef85d5f6c11cee80
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 18:27:29 -0300

    feat: add update product validator factory

---
+++ src/main/factories/validations/product/update-product.ts
@@ -0,0 +1,12 @@
+import { Validator } from "../../../../validation/contracts/validator"
+import { RequiredFieldValidator } from "../../../../validation/validators/required-field"
+import { ValidatorComposite } from "../../../../validation/validators/validator.composite"
+
+export const makeUpdateProductValidator = (): ValidatorComposite => {
+    const validations: Validator[] = []
+    const requiredFields = ['id', 'idCategory', 'name', 'image']
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidator(field))
+    }
+    return new ValidatorComposite(validations)
+  }
