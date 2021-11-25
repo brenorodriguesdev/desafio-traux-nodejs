3cd16c289adf0bd82bdbc4bdacedc74eb6c62f7b
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 11:41:00 -0300

    fix: get all category usecase

--- src/domain/useCases/category/get-all-category.ts
+++ src/domain/useCases/category/get-all-category.ts
@@ -1,5 +1,5 @@
 import { CategoryModel } from "../../models/category/category";
 
 export interface GetAllCategoryUseCase {
-    get: (id: number) => Promise<CategoryModel[]>
+    get: () => Promise<CategoryModel[]>
 }
