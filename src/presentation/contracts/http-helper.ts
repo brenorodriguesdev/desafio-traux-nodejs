f717265e1e2cc7c3f0f07055e1c6c3ad42616245
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 11:15:37 -0300

    fix: http helper

---
+++ src/presentation/contracts/http-helper.ts
@@ -0,0 +1,32 @@
+import { ServerError } from "../errors/server-error";
+import { HttpResponse } from "./http";
+
+export const serverError = (): HttpResponse => ({
+    data: new ServerError(),
+    statusCode: 500
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+    data: error,
+    statusCode: 400
+})
+
+export const unauthorized = (error: Error): HttpResponse => ({
+    data: error,
+    statusCode: 401
+})
+
+export const ok = (data: any): HttpResponse => ({
+    data,
+    statusCode: 200
+})
+
+export const noContent = (): HttpResponse => ({
+    data: null,
+    statusCode: 201
+})
+
+export const noFound = (): HttpResponse => ({
+    data: null,
+    statusCode: 201
+})
