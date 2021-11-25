f3ba7c19a69bfd6962a9d20b9f05a33cf6e09b61
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 18:25:42 -0300

    feat: add get by id category validator factory

---
+++ src/main/factories/validations/category/get-by-id-category.ts
@@ -0,0 +1,12 @@
+import { Validator } from "../../../../validation/contracts/validator"
+import { RequiredFieldValidator } from "../../../../validation/validators/required-field"
+import { ValidatorComposite } from "../../../../validation/validators/validator.composite"
+
+export const makeGetByIdCategoryValidator = (): ValidatorComposite => {
+    const validations: Validator[] = []
+    const requiredFields = ['id']
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidator(field))
+    }
+    return new ValidatorComposite(validations)
+  }
