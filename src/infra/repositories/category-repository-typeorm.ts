3558539780321d8506d94f4dd786861875c89b91
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 13:47:04 -0300

    feat: implements category repository

---
+++ src/infra/repositories/category-repository-typeorm.ts
@@ -0,0 +1,32 @@
+import { getRepository } from 'typeorm'
+import { CategoryRepository } from '../../data/contracts/repositories/category-repository'
+import { Category } from '../../data/entities/category'
+
+export class CategoryRepositoryTypeORM implements CategoryRepository {
+    async getAll (): Promise<Category[]> {
+      const categoryRepository = getRepository(Category)
+      return await categoryRepository.find()
+    }
+  
+    async getById (id: number): Promise<Category> {
+      const categoryRepository = getRepository(Category)
+      return await categoryRepository.findOne(id)
+    }
+  
+    async deleteById (id: number): Promise<void> {
+      const categoryRepository = getRepository(Category)
+      await categoryRepository.delete(id)
+    }
+  
+    async create (category: Category): Promise<Category> {
+      const categoryRepository = getRepository(Category)
+      return await categoryRepository.save(category)
+    }
+  
+    async update (category: Category): Promise<void> {
+      const categoryRepository = getRepository(Category)
+      const { id } = category
+      delete category.id
+      await categoryRepository.update({ id }, category)
+    }
+  }
