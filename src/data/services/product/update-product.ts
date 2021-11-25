f17371b9d1d4ac02c8684d645a8cb1de02310e6f
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 13:25:41 -0300

    feat: add update product service

---
+++ src/data/services/product/update-product.ts
@@ -0,0 +1,22 @@
+import { UpdateProductModel } from "../../../domain/models/product/update-product";
+import { UpdateProductUseCase } from "../../../domain/useCases/product/update-product";
+import { CategoryRepository } from "../../contracts/repositories/category-repository";
+import { ProductRepository } from "../../contracts/repositories/product-repository";
+
+export class UpdateProductService implements UpdateProductUseCase {
+    constructor(private readonly categoryRepository: CategoryRepository, private readonly productRepository: ProductRepository) { }
+    async update(data: UpdateProductModel): Promise<void | Error> {
+        const category = await this.categoryRepository.getById(data.idCategory)
+        if (!category) {
+            return new Error('category not found')
+        }
+        
+        const product = await this.productRepository.update({
+            id: data.id,
+            category,
+            name: data.name,
+            image: data.image
+        })
+
+    }
+}
