6462e9e0df36fba0052431cfb4c915c4ab6b802e
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 18:20:32 -0300

    feat: add sign up validator factory

---
+++ src/main/factories/validations/login/sign-up.ts
@@ -0,0 +1,12 @@
+import { Validator } from "../../../../validation/contracts/validator"
+import { RequiredFieldValidator } from "../../../../validation/validators/required-field"
+import { ValidatorComposite } from "../../../../validation/validators/validator.composite"
+
+export const makeSignUpValidator = (): ValidatorComposite => {
+    const validations: Validator[] = []
+    const requiredFields = ['name', 'username', 'password']
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidator(field))
+    }
+    return new ValidatorComposite(validations)
+  }
