db40ab1e6d6c696c35174ee7d6b45793fff281f5
“Breno <brenorodriguesdev@gmail.com>
2021-11-25 13:13:42 -0300

    feat: add update category service

---
+++ src/data/services/category/update-category.ts
@@ -0,0 +1,14 @@
+import { UpdateCategoryModel } from "../../../domain/models/category/update-category";
+import { UpdateCategoryUseCase } from "../../../domain/useCases/category/update-category";
+import { CategoryRepository } from "../../contracts/repositories/category-repository";
+
+export class UpdateCategoryService implements UpdateCategoryUseCase {
+    constructor(private readonly categoryRepository: CategoryRepository) { }
+    async update(data: UpdateCategoryModel): Promise<void | Error> {
+        const category = await this.categoryRepository.getById(data.id)
+        if (!category) {
+            return new Error('category not found')
+        }
+        await this.categoryRepository.update(data)
+    }
+}
