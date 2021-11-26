356cb6fa383791e539eb42f73b0268cf809f3505
“Breno <brenorodriguesdev@gmail.com>
2021-11-26 01:53:07 -0300

    feat: implements decrypter

--- src/infra/cryptography/jsonwebtoken.ts
+++ src/infra/cryptography/jsonwebtoken.ts
@@ -1,10 +1,16 @@
 import { Encrypter } from '../../data/contracts/cryptography/encrypter';
+import { Decrypter } from '../../data/contracts/cryptography/decrypter';
+
 import jwt from 'jsonwebtoken';
 
-export class JsonWebToken implements Encrypter {
+export class JsonWebToken implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (plaintext: string): Promise<string> {
     return jwt.sign({ id: plaintext }, this.secret)
+  }
+
+  async decrypt (ciphertext: string): Promise<string> {
+    return jwt.verify(ciphertext, this.secret) as any
   }
 }
