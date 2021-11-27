4b51c0867d67e0b74228f908a4adcd85dd6975b2
“Breno <brenorodriguesdev@gmail.com>
2021-11-27 18:20:11 -0300

    feat: add setup upload file

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,11 +2,13 @@ import cors from 'cors'
 import express from 'express'
 import setupRoutes from './routes'
 import setupSwagger from './swagger'
+import path from 'path'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use('/uploads', express.static(path.join(__dirname, '..', '..', 'uploads')))
 setupSwagger(app)
 setupRoutes(app)
 
